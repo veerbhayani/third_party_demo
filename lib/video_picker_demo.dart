b37f53345941dc1732cf5086981176a08223e33e
veer bhayani <virbhayani137@gmail.com>
2023-07-25 13:16:56 +0530

    video player demo

--- lib/video_picker_demo.dart
+++ lib/video_picker_demo.dart
@@ -12,77 +12,143 @@ class VideoPickerDemo extends StatefulWidget {
 }
 
 class _VideoPickerDemoState extends State<VideoPickerDemo> {
-  late VideoPlayerController _videoPlayerController;
+  late VideoPlayerController _controller;
+  bool isVideoSelected = false;
+  bool isPlaying = false;
+  bool isMuted = false;
   File? file;
   Future<void> selectVideo() async {
     ImagePicker pickVideo = ImagePicker();
     final xFile = await pickVideo.pickVideo(
       source: ImageSource.gallery,
     );
-    file = File(xFile!.path);
-    setState(() {});
-  }
-
-  @override
-  void initState() {
-    _videoPlayerController = VideoPlayerController.file(file!);
-    super.initState();
-  }
-
-  @override
-  void dispose() {
-    _videoPlayerController.dispose();
-    super.dispose();
+    if (xFile != null) {
+      file = File(xFile.path);
+      _controller = VideoPlayerController.file(file!)
+        ..initialize().then((_) {
+          setState(() {});
+          _controller.play();
+        });
+    }
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Column(
-          children: [
-            Center(
-              child: MaterialButton(
-                onPressed: () {
-                  selectVideo();
-                  setState(() {});
-                },
-                color: Colors.blue,
-                child: const Text(
-                  'Select Video',
-                  style: TextStyle(
-                    color: Colors.white,
-                  ),
+      appBar: AppBar(
+        title: const Text(
+          'Video Player Demo',
+        ),
+        actions: [
+          TextButton(
+            onPressed: () async {
+              await selectVideo();
+              if (file != null) {
+                isVideoSelected = true;
+              } else {
+                isVideoSelected = false;
+              }
+              setState(() {});
+            },
+            child: const Text(
+              'Select Video',
+              style: TextStyle(
+                color: Colors.white,
+              ),
+            ),
+          ),
+          const SizedBox(
+            width: 10,
+          ),
+        ],
+      ),
+      body: ListView(
+        scrollDirection: Axis.vertical,
+        children: [
+          if (file != null && isVideoSelected == true)
+            _controller.value.isInitialized
+                ? AspectRatio(
+                    aspectRatio: _controller.value.aspectRatio,
+                    child: VideoPlayer(
+                      _controller,
+                    ),
+                  )
+                : Container()
+          else
+            const Padding(
+              padding: EdgeInsets.all(
+                15,
+              ),
+              child: Center(
+                child: Text(
+                  'No Video Selected',
                 ),
               ),
             ),
-            const SizedBox(
-              height: 20,
-            ),
-            _videoPlayerController.value.isInitialized
-                ? AspectRatio(
-                    aspectRatio: _videoPlayerController.value.aspectRatio,
-                    child: VideoPlayer(
-                      _videoPlayerController,
+          isVideoSelected
+              ? Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    isPlaying
+                        ? IconButton(
+                            onPressed: () async {
+                              isPlaying = false;
+                              await _controller.play();
+                              setState(() {});
+                            },
+                            icon: const Icon(
+                              Icons.play_arrow_rounded,
+                            ),
+                          )
+                        : IconButton(
+                            onPressed: () async {
+                              await _controller.pause();
+                              isPlaying = true;
+                              setState(() {});
+                            },
+                            icon: const Icon(
+                              Icons.pause,
+                            ),
+                          ),
+                    IconButton(
+                      onPressed: () {
+                        _controller.dispose();
+                        isVideoSelected = false;
+                        setState(() {});
+                      },
+                      icon: const Icon(
+                        Icons.stop,
+                      ),
                     ),
-                  )
-                : const Text('No video selected'),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          setState(() {
-            _videoPlayerController.value.isPlaying
-                ? _videoPlayerController.pause()
-                : _videoPlayerController.play();
-          });
-        },
-        child: Icon(
-          _videoPlayerController.value.isPlaying
-              ? Icons.pause
-              : Icons.play_arrow,
-        ),
+                    isMuted
+                        ? IconButton(
+                            onPressed: () {
+                              _controller.setVolume(
+                                200,
+                              );
+                              isMuted = !isMuted;
+                              setState(() {});
+                            },
+                            icon: const Icon(
+                              Icons.volume_off,
+                            ),
+                          )
+                        : IconButton(
+                            onPressed: () {
+                              _controller.setVolume(
+                                0,
+                              );
+                              isMuted = !isMuted;
+                              setState(() {});
+                            },
+                            icon: const Icon(
+                              Icons.volume_up,
+                            ),
+                          ),
+                  ],
+                )
+              : Container(),
+        ],
       ),
     );
   }
